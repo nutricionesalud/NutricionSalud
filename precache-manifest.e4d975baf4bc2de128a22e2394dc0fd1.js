8badbf1902a4427d8489341aa79c4cd24b10e053
nutricionesalud <nysalud10@gmail.com>
2020-12-14 15:53:56 -0500

    Updates

---
+++ precache-manifest.e4d975baf4bc2de128a22e2394dc0fd1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e680b8952d79f04dc0642fc01525b7",
+    "url": "/NutricionSalud/index.html"
+  },
+  {
+    "revision": "3c51c1cee9076106b1c0",
+    "url": "/NutricionSalud/static/css/main.b9660a61.chunk.css"
+  },
+  {
+    "revision": "8d59f4b28622c9e707c8",
+    "url": "/NutricionSalud/static/js/2.b11e54cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NutricionSalud/static/js/2.b11e54cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c51c1cee9076106b1c0",
+    "url": "/NutricionSalud/static/js/main.a03b2db8.chunk.js"
+  },
+  {
+    "revision": "e734a3832b3153f362a5",
+    "url": "/NutricionSalud/static/js/runtime-main.df1a441c.js"
+  },
+  {
+    "revision": "a58d9db1e37e62f61a9cc38f0a13c3b3",
+    "url": "/NutricionSalud/static/media/Empanadas.a58d9db1.jpg"
+  },
+  {
+    "revision": "75d46783a56ee502866a2561d8449b0f",
+    "url": "/NutricionSalud/static/media/alfajor.75d46783.jpg"
+  },
+  {
+    "revision": "2f8cd6a108b5923598260976cd767656",
+    "url": "/NutricionSalud/static/media/alfajores.2f8cd6a1.jpg"
+  },
+  {
+    "revision": "6cae6aad6390cb5bab9bb478672bdc82",
+    "url": "/NutricionSalud/static/media/bocaditoicon.6cae6aad.png"
+  },
+  {
+    "revision": "0e1414fbeeb5174054530d7f462b7a2d",
+    "url": "/NutricionSalud/static/media/contactenos.0e1414fb.jpg"
+  },
+  {
+    "revision": "fb7c82c53bb666d8e35c010702447547",
+    "url": "/NutricionSalud/static/media/empanada.fb7c82c5.jpg"
+  },
+  {
+    "revision": "7cc1e07cad56de9fefa93fb297720fc0",
+    "url": "/NutricionSalud/static/media/empandaicon.7cc1e07c.png"
+  },
+  {
+    "revision": "e00d1f1411771855edc0df4a56e6ef86",
+    "url": "/NutricionSalud/static/media/keke.e00d1f14.jpg"
+  },
+  {
+    "revision": "8cb685b6e99524aab687c2a16f5e7ef0",
+    "url": "/NutricionSalud/static/media/keke3.8cb685b6.jpg"
+  },
+  {
+    "revision": "176c086f53550a02f22f7ce732cb2e3d",
+    "url": "/NutricionSalud/static/media/kekeicon.176c086f.png"
+  },
+  {
+    "revision": "4d74952b61c3762448d952bf99aec5d2",
+    "url": "/NutricionSalud/static/media/pastel.4d74952b.jpg"
+  },
+  {
+    "revision": "71ba6ae338989e8967c34d5ed7b94c5f",
+    "url": "/NutricionSalud/static/media/pastelicon.71ba6ae3.png"
+  }
+]);
